4367a5b0c401dcb5bb43a638e715234e1354cedf
JoaoGuiMB <juaogui159@gmail.com>
2023-09-02 13:32:37 -0300

    feat: add env

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "ws://localhost:1337/";
+const SERVER_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:1337/";
 
 export const socket = io(SERVER_URL, { transports: ["websocket"] });
